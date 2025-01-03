b10bb0e656c668022c63a1425e3294c3aeb8e806
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2025-01-03 12:19:07 +0530

    test 4

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ dotenv.config();
 const app = express();
 
 // Middleware
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  })
+);
+// app.use(cors());
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
